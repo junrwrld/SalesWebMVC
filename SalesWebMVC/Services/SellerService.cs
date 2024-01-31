243d567cd2177b6b5bf6e5bdb41b72b519b949eb
junrwrld <batistasousajoao@gmail.com>
2024-01-31 15:27:35 -0300

    Implementing SimpleSearch

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -41,7 +41,7 @@ namespace SalesWebMVC.Services
                 _context.Seller.Remove(obj);
                 await _context.SaveChangesAsync();
             }
-            catch(DbUpdateException e)
+            catch(DbUpdateException)
             {
                 throw new IntegrityException("Can't be delete seller because he/she has sales");
             }
